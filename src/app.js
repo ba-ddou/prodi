b3f6ed6990098bca977d372d134c53653f000f1a
badou abdelbassite <abdelbassite.badou@gmail.com>
2019-11-03 16:30:05 +0100

    file structure 101

--- src/app.js
+++ src/app.js
@@ -3,3 +3,14 @@
 *
 *
 */
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('request received on GET /');
+    res.status(200);
+    res.send('hello, world!');
+});
+
+app.listen(3000, _ => console.log('server is listening on port 3000'));
---
+++ src/subscribers/map.js
@@ -0,0 +1,8 @@
+/*
+*
+*   List of all events and their corresponding listenners
+*   Defines which event should each listenner subscribe to
+*
+*
+*
+*/
